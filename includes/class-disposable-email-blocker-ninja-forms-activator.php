ea22205580e1553cd12578cbd0b98ad878ed2f51
Sajjad Hossain Sagor <sagorh672@gmail.com>
2025-05-19 13:14:33 +0600

    fixed spacing alignments

--- includes/class-disposable-email-blocker-ninja-forms-activator.php
+++ includes/class-disposable-email-blocker-ninja-forms-activator.php
@@ -24,6 +24,7 @@ class Disposable_Email_Blocker_Ninja_Forms_Activator {
 	 * with data from a txt file.
 	 *
 	 * @since     2.0.0
+	 * @static
 	 * @access    public
 	 */
 	public static function on_activate() {
--- includes/class-disposable-email-blocker-ninja-forms-deactivator.php
+++ includes/class-disposable-email-blocker-ninja-forms-deactivator.php
@@ -24,6 +24,7 @@ class Disposable_Email_Blocker_Ninja_Forms_Deactivator {
 	 * or removing options.
 	 *
 	 * @since     2.0.0
+	 * @static
 	 * @access    public
 	 */
 	public static function on_deactivate() {}
